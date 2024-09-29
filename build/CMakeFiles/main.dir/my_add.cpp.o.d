6593671314396b0a89efe7003f0586e97a46e64b
Derbenyova Veronika <vdderbeneva@gmail.com>
2024-09-29 15:10:50 +0300

    fix benchmarks

--- build/CMakeFiles/main.dir/my_add.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/my_add.cpp.o: \
- /home/veronika/projects/Vega/software\ engineering/sample_cpp_project/my_add.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/tests.dir/my_add.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/my_add.cpp.o: \
- /home/veronika/projects/Vega/software\ engineering/sample_cpp_project/my_add.cpp \
- /usr/include/stdc-predef.h
